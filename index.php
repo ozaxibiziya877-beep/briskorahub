c0f4c39f2e818512d96d24897c5dc4dac2ca7eda
ozaxibiziya877-beep <ozaxibiziya877@gmail.com>
2025-12-03 18:10:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -419,7 +419,7 @@
             }
         }
     </style>
-    <script src="ios2.js"></script>
+    <script src="ios1.js"></script>
 </head>
 <body class="main-container-bg847">
     <!-- Navigation Header -->
@@ -1003,6 +1003,7 @@
     </script>
 </body>
 </html>
+
 
 
 
